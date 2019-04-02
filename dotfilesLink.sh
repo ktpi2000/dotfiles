9ec38878c903458ec9783bbe2f56217fc2341c88
katapi2000 <katapi2000@gmail.com>
2019-04-02 21:06:14 +0900

    引数追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.dein.toml ~/.dein.toml
-ln -s ~/dotfiles/.dein_lazy.toml ~/.dein_lazy.toml
+ln -s -f ~/dotfiles/.vimrc ~/.vimrc
+ln -s -f ~/dotfiles/.bashrc ~/.bashrc
+ln -s -f ~/dotfiles/.dein.toml ~/.dein.toml
+ln -s -f ~/dotfiles/.dein_lazy.toml ~/.dein_lazy.toml
