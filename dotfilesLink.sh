ab6e4ba31e3b28cf0f64c9cf9e059564c7e8ae15
katapi2000 <katapi2000@gmail.com>
2019-04-18 01:42:20 +0900

    変更

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,4 @@ ln -s -f ~/dotfiles/.vimrc ~/.vimrc
 ln -s -f ~/dotfiles/.bashrc ~/.bashrc
 ln -s -f ~/dotfiles/.dein.toml ~/.dein.toml
 ln -s -f ~/dotfiles/.dein_lazy.toml ~/.dein_lazy.toml
+ln -s -f ~/dotfiles/.tmux.conf ~/.tmux.conf
